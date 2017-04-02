0034a60c297943d649b1dc7a6bf3f3218d8d1ce4
James Kyle <me@thejameskyle.com>
2017-04-02 15:08:13 -0700

    Init commit

---
+++ src/definitions/experimental.js
@@ -0,0 +1,95 @@
+import defineType, { assertNodeType } from "./index";
+
+defineType("AwaitExpression", {
+  builder: ["argument"],
+  visitor: ["argument"],
+  aliases: ["Expression", "Terminatorless"],
+  fields: {
+    argument: {
+      validate: assertNodeType("Expression"),
+    }
+  }
+});
+
+defineType("ForAwaitStatement", {
+  visitor: ["left", "right", "body"],
+  aliases: ["Scopable", "Statement", "For", "BlockParent", "Loop", "ForXStatement"],
+  fields: {
+    left: {
+      validate: assertNodeType("VariableDeclaration", "LVal")
+    },
+    right: {
+      validate: assertNodeType("Expression")
+    },
+    body: {
+      validate: assertNodeType("Statement")
+    }
+  }
+});
+
+defineType("BindExpression", {
+  visitor: ["object", "callee"],
+  aliases: ["Expression"],
+  fields: {
+    // todo
+  }
+});
+
+defineType("Decorator", {
+  visitor: ["expression"],
+  fields: {
+    expression: {
+      validate: assertNodeType("Expression")
+    }
+  }
+});
+
+defineType("DoExpression", {
+  visitor: ["body"],
+  aliases: ["Expression"],
+  fields: {
+    body: {
+      validate: assertNodeType("BlockStatement")
+    }
+  }
+});
+
+defineType("ExportDefaultSpecifier", {
+  visitor: ["exported"],
+  aliases: ["ModuleSpecifier"],
+  fields: {
+    exported: {
+      validate: assertNodeType("Identifier")
+    }
+  }
+});
+
+defineType("ExportNamespaceSpecifier", {
+  visitor: ["exported"],
+  aliases: ["ModuleSpecifier"],
+  fields: {
+    exported: {
+      validate: assertNodeType("Identifier")
+    }
+  }
+});
+
+defineType("RestProperty", {
+  visitor: ["argument"],
+  aliases: ["UnaryLike"],
+  fields: {
+    argument: {
+      validate: assertNodeType("LVal")
+    }
+  }
+});
+
+defineType("SpreadProperty", {
+  visitor: ["argument"],
+  aliases: ["UnaryLike"],
+  fields: {
+    argument: {
+      validate: assertNodeType("Expression")
+    }
+  }
+});
---
+++ src/definitions/misc.js
@@ -0,0 +1,15 @@
+import defineType, { assertNodeType } from "./index";
+
+defineType("Noop", {
+  visitor: []
+});
+
+defineType("ParenthesizedExpression", {
+  visitor: ["expression"],
+  aliases: ["Expression", "ExpressionWrapper"],
+  fields: {
+    expression: {
+      validate: assertNodeType("Expression")
+    }
+  }
+});
